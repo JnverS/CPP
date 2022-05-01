c8a39abeff4c15bf32f3f8c9dc0dc1ff8e0dded7
kdominic <jnvers@mail.ru>
2022-05-01 18:17:05 +0300

    cpp00_ex_01

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -3,18 +3,17 @@
 int main() {
     std::string cmd;
 
-    std::cout << "Введите команду: ";
-    getline(std::cin, cmd);
+    while (1) {
+        std::cout << "Введите команду: ";
+        getline(std::cin, cmd);
 
-    switch (cmd) {
-        case "ADD":
-            ; //save new contact
-        case "SEARCH":
-            ; //view contact
-        case "EXIT":
-            ; //end of programm
-        default:
-            std::cout << cmd;
+        if (cmd == "ADD") {
+            std::cout << cmd << " save new contact" << std::endl;
+        } else if (cmd == "SEARCH") {
+            std::cout << cmd << " find contact" << std::endl;
+        } else if (cmd == "EXIT") {
+            std::cout << cmd << " end the programm" << std::endl;
+        }
     }
 
     return 0;
