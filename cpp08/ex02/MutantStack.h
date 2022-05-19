8ef4e5c0bd739f9e8cfe82ce22588df1b2090c92
kdominic <jnvers@mail.ru>
2022-05-19 17:54:48 +0300

    ex02

--- cpp08/ex02/MutantStack.h
+++ cpp08/ex02/MutantStack.h
@@ -1,7 +1,18 @@
-
 #ifndef MY_GITCLONE_MUTANTSTACK_H
 #define MY_GITCLONE_MUTANTSTACK_H
 
+#include <iostream>
 
+template <class Type, class Container = deque<int>>
+class MutantStack{
+public:
+    MutantStack();
+    MutantStack(const MutantStack&);
+    ~MutantStack();
+
+    MutantStack& operator = (const MutantStack&);
+
+
+};
 
 #endif //MY_GITCLONE_MUTANTSTACK_H
